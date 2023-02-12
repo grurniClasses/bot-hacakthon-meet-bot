4957ada766e31664647d768531bf9d6f56d239f9
Netaly Klemann <netalyk@mail.tau.ac.il>
2023-02-12 16:05:42 +0200

    added message to start meeting

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import logging
-from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from pprint import pprint
+
+from telegram import Update
 from telegram.ext import (
     CommandHandler,
     CallbackContext,
@@ -10,17 +12,10 @@ from telegram.ext import (
 from pymongo import MongoClient
 
 import bot_settings
-
+bot_name = 'GiliTheKingBot'
 client = MongoClient()
 db = client.get_database("meetingDB")
 meetings = db.get_collection("meetings")
-meeting = {
-    'meeting_id':"",
-    'dates': "2"
-}
-
-
-
 
 logging.basicConfig(
     format="[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s",
@@ -31,15 +26,15 @@ logger = logging.getLogger(__name__)
 
 WELCOME_MESSAGE = "Hey there! when do you want to organize your meeting?"
 MEETING_HOST = "marina"
-MEETING_MESSAGE = f'you are invited by {MEETING_HOST} to a meeting. {MEETING_HOST} has chosen this dates for the meeting {meeting["dates"]}'
+# MEETING_MESSAGE = f'you are invited by {MEETING_HOST} to a meeting. {MEETING_HOST} has chosen this dates for the meeting {meeting["dates"]}'
 
 def start(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     logger.info(f"> Start chat #{chat_id}")
     if len(update.message.text.split()) > 1:
         meeting_id = update.message.text.split()[1]
-        print(meeting)
-        context.bot.send_message(chat_id=chat_id, text=MEETING_MESSAGE)
+        # print(meeting)
+        # context.bot.send_message(chat_id=chat_id, text=MEETING_MESSAGE)
         meetings.find()
 
     else:
@@ -49,19 +44,16 @@ def start(update: Update, context: CallbackContext):
 
 def respond(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Just a second...",)
     logger.info(f"= Got on chat #{chat_id}")
-    user_dates = update.message.text.split(",")
-    meeting["dates"] = user_dates
-    meeting['meeting_id'] = chat_id
-    meetings.insert_one(meeting)
-    print(meeting)
-
-
-
-
-
-
+    meeting={
+        'dates':update.message.text.split(','),
+        'meeting_id':chat_id
+    }
+    result = meetings.insert_one(meeting)
+    url_req = f"https://t.me/{bot_name}?start={result.inserted_id}"
+    context.bot.send_message(chat_id=chat_id, text='Please forward the follow message to your guests')
+    meeting_message = f'You are invited by {update.message.chat.first_name} to a meeting. \b Follow the link to see the invitation {url_req}'
+    context.bot.send_message(chat_id=chat_id, text=meeting_message)
 
 my_bot = Updater(token=bot_settings.BOT_TOKEN, use_context=True)
 my_bot.dispatcher.add_handler(CommandHandler("start", start))
