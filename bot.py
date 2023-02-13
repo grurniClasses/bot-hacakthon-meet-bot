e92b91a7375b26cd889265525d5df8371e807bba
Netaly Klemann <netalyk@mail.tau.ac.il>
2023-02-13 15:32:36 +0200

    update

--- bot.py
+++ bot.py
@@ -2,12 +2,8 @@ import logging
 import random
 import string
 from pprint import pprint
-
 import bot_settings
-
 from telegram import  ReplyKeyboardRemove
-
-
 
 from telegram import Update
 from telegram.ext import (
@@ -34,8 +30,8 @@ logger = logging.getLogger(__name__)
 
 WELCOME_MESSAGE = "Hey there! \bwhen do you want to organize your meeting?"
 
-
 def start(update: Update, context: CallbackContext):
+    context.user_data["dates"] = {}
     chat_id = update.effective_chat.id
     logger.info(f"> Start chat #{chat_id}")
     if len(update.message.text.split()) > 1:
@@ -43,7 +39,6 @@ def start(update: Update, context: CallbackContext):
         data = meetings.find_one({'code': code})
         meetings_message = f'Please select the dates you can from the flows dates : {data["dates"]}'
         context.bot.send_message(chat_id=chat_id, text=meetings_message)
-        context.user_data['code']=code
     else:
         calendar_handler(update,context)
 
@@ -112,19 +107,38 @@ def callback_handler(update: Update, context: CallbackContext):
     data = query.data
     logger.info(f"> CALLBACK #{chat_id}, {data=}")
     query.answer()
-    cmd = data.split(":")[0]
-    if cmd == "start" and len(cmd) == 0:
-        # n = int(data.split(":")[1])
-        # d = context.user_data.get("counters", {})
-        # d[n] = d.get(n, 0) + 1
-        # context.user_data["counters"] = d
-        # reply_markup = get_counters_keyboard(d)
+    cmd = data.split(";")
+    if cmd[0] == "DAY":
+        date = f'{cmd[3]}/{cmd[2]}/{cmd[1]}'
+        d = context.user_data.get("dates", {})
+        d[date] = d.get(date, 0) + 1
+        context.user_data["dates"] =d
         query.edit_message_text(text=f"{data}", reply_markup=telegramcalendar.create_calendar())
+        context.bot.send_message(chat_id=update.callback_query.from_user.id,
+                                 text="You selected %s" % (date))
+
+def end(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    code = get_random_code()
+    logger.info(f"= Got on chat #{chat_id},{code=}")
+    meeting = {
+        'dates': context.user_data["dates"],
+        'createre_chat_id': chat_id,
+        'code':code
+    }
+    result = meetings.insert_one(meeting)
+    url_req = f"https://t.me/{bot_settings.BOT_NAME}?start={code}"
+    context.bot.send_message(chat_id=chat_id, text='Please forward the follow message to your guests')
+    meeting_message = f'You are invited by {update.message.chat.first_name} to a meeting. \b Follow the link to see the invitation {url_req}'
+    context.bot.send_message(chat_id=chat_id, text=meeting_message)
 
 
 my_bot = Updater(token=bot_settings.BOT_TOKEN, use_context=True)
 my_bot.dispatcher.add_handler(CommandHandler("start", start))
 my_bot.dispatcher.add_handler(CommandHandler("status", status))
+my_bot.dispatcher.add_handler(CommandHandler("end", end))
+my_bot.dispatcher.add_handler(CallbackQueryHandler(callback_handler))
+
 # my_bot.dispatcher.add_handler(CommandHandler("calendar", calendar_handler))
 # my_bot.dispatcher.add_handler(CallbackQueryHandler(inline_handler))
 # my_bot.dispatcher.add_handler(MessageHandler(Filters.text, respond))
