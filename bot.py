cc4c8ffd93b0a0c73cc7d9886024daa5a415d97c
Netaly Klemann <netalyk@mail.tau.ac.il>
2023-02-14 09:13:19 +0200

    .

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 import logging
 import random
 import string
-from pprint import pprint
-
 import bot_settings
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ParseMode
 from telegram.ext import (
     CommandHandler,
     CallbackContext,
@@ -25,7 +23,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 WELCOME_INVITER_MESSAGE = "Hey there! \nChoose date options to organize your meeting \nLet me know you are done by sending /end or pressing the submit button"
-SUBMIT_BUTTON = 'Let me know when you are done by sending /end or pressing the submit button'
+SUBMIT_BUTTON = 'Let me know when you are done by pressing the submit button'
 GUEST_MESSAGE = 'Please select the dates you can from the following dates : '
 
 
@@ -34,7 +32,7 @@ def get_random_code(k=16):
 
 
 def start(update: Update, context: CallbackContext):
-    context.user_data["dates"] = {}
+    context.user_data["dates"] = []
     chat_id = update.effective_chat.id
     logger.info(f"> Start chat #{chat_id}")
     if len(update.message.text.split()) > 1:
@@ -50,10 +48,13 @@ def start(update: Update, context: CallbackContext):
 
 def create_option_button(context: CallbackContext):
     code = context.user_data['code']
+    dates = context.user_data['dates']
     data = meetings.find_one({'code': code})
-    options_list = list(data["dates"].keys())
+    options_list = data["dates"]
     button_options_list = [
-        [InlineKeyboardButton(f"{option}", callback_data=f"option:{option}")] for option in options_list
+        [InlineKeyboardButton(f"{option} {'✔' if option in dates else ' '}", callback_data=f"option:{option}")] for
+        option in
+        options_list
     ]
     reply_markup = InlineKeyboardMarkup(button_options_list)
     return reply_markup
@@ -66,13 +67,13 @@ def guess_schedule_handler(update: Update, context: CallbackContext):
         [InlineKeyboardButton("Submit", callback_data="guess_submit")]
     ]
     reply_markup = InlineKeyboardMarkup(submit_button)
-    # todo end not working!
     context.bot.send_message(chat_id=chat_id, text=SUBMIT_BUTTON, reply_markup=reply_markup)
 
 
 def calendar_handler(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
-    update.message.reply_text(WELCOME_INVITER_MESSAGE, reply_markup=telegramcalendar.create_calendar())
+    update.message.reply_text(WELCOME_INVITER_MESSAGE,
+                              reply_markup=telegramcalendar.create_calendar(context.user_data["dates"]))
     submit_button = [
         [InlineKeyboardButton("Submit", callback_data="inviter_submit")]
     ]
@@ -94,10 +95,8 @@ def callback_handler(update: Update, context: CallbackContext):
         update_inviter_dates(update, context)
     if cmd[0] == 'inviter_submit':
         inviter_submit(update, context, query.message.chat.first_name)
-        # context.bot.dele(chat_id=chat_id,message_id=)
     if cmd[0] == 'guess_submit':
         guess_submit(update, context)
-        # context.bot.deleteMessage(chat_id=chat_id,message_id=)
 
 
 def update_guess_dates(update: Update, context: CallbackContext):
@@ -105,32 +104,37 @@ def update_guess_dates(update: Update, context: CallbackContext):
     data = query.data
     chat_id = query.from_user.id
     chosen_dates = data.split(":")[1]
-    d = context.user_data.get("dates", {})
-    d[chosen_dates] = d.get(chosen_dates, 0) + 1
-    context.user_data["dates"] = d
-    context.bot.send_message(chat_id, text=f"your selected {chosen_dates}")
-    query.edit_message_text(text=GUEST_MESSAGE,
-                            reply_markup=create_option_button(context))
+    li = context.user_data.get("dates")
+    if chosen_dates not in li:
+        li.append(chosen_dates)
+        context.user_data["dates"] = li
+    else:
+        li.remove(chosen_dates)
+    query.edit_message_text(text=GUEST_MESSAGE, reply_markup=create_option_button(context))
 
 
 def update_inviter_dates(update: Update, context: CallbackContext):
     query = update.callback_query
-    selected, date = telegramcalendar.process_calendar_selection(update, context)
+    selected, date = telegramcalendar.process_calendar_selection(context.user_data["dates"], update, context)
     if selected:
         date = date.strftime("%d/%m/%Y")
-        d = context.user_data.get("dates", {})
-        d[date] = d.get(date, 0) + 1
-        context.user_data["dates"] = d
-        context.bot.send_message(chat_id=update.callback_query.from_user.id, text="You selected %s" % (date))
-        query.edit_message_text(text=WELCOME_INVITER_MESSAGE, reply_markup=telegramcalendar.create_calendar())
+        li = context.user_data.get("dates")
+        if date not in li:
+            li.append(date)
+            context.user_data["dates"] = li
+        else:
+            li.remove(date)
+        query.edit_message_text(text=WELCOME_INVITER_MESSAGE,
+                                reply_markup=telegramcalendar.create_calendar(context.user_data["dates"]))
 
 
 def status(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     dates = meetings.find_one({'code': context.user_data['code']})["dates"]
-    context.bot.send_message(chat_id=chat_id, text="Your guests chose:")
+    guesses = meetings.find_one({'code': context.user_data['code']})["guesses"]
+    context.bot.send_message(chat_id=chat_id, text=f"Your guests chose: ({len(guesses)} answered)")
     for k, v in dates.items():
-        thumbs = (v - 1) * "👍"
+        thumbs = v * "👍"
         context.bot.send_message(chat_id=chat_id, text=f"{k}: {thumbs}")
 
 
@@ -140,18 +144,21 @@ def inviter_submit(update: Update, context: CallbackContext, name=None):
     chat_id = update.effective_chat.id
     code = get_random_code()
     logger.info(f"= Got on chat #{chat_id},{code=}")
+    dic = {}
+    for date in context.user_data["dates"]:
+        dic[date] = 0
     meeting = {
-        'dates': context.user_data["dates"],
+        'dates': dic,
         'code': code,
         'guesses': []
     }
     meetings.insert_one(meeting)
     url_req = f"https://t.me/{bot_settings.BOT_NAME}?start={code}"
-    context.bot.send_message(chat_id=chat_id, text='Please forward the following message to your guests')
-    meeting_message = f'You are invited by {name} to a meeting. \n Follow the link to see the invitation {url_req} and press *START*'
-    context.bot.send_message(chat_id=chat_id, text=meeting_message)
+    update.callback_query.edit_message_text(text='Please forward the following message to your guests')
+    meeting_message = f'You are invited by {name} to a meeting. \nFollow the link to see the invitation {url_req} and press *START*'
+    context.bot.send_message(chat_id=chat_id, text=meeting_message, parse_mode=ParseMode.MARKDOWN)
     context.bot.send_message(chat_id=chat_id,
-                             text='Give them a little while to answer. send /status to see what they chose')
+                             text='Give them a little while to answer. Send /status to see what they chose')
 
 
 def guess_submit(update: Update, context: CallbackContext):
@@ -160,12 +167,12 @@ def guess_submit(update: Update, context: CallbackContext):
         dates[date] += 1
     meetings.update_one({'code': context.user_data['code']}, {"$set": {'dates': dates}})
     meetings.update_one({'code': context.user_data['code']}, {'$push': {'guesses': update.effective_chat.id}})
+    update.callback_query.edit_message_text("Your answers been submitted. \nSend /status to see the other answers")
 
 
 my_bot = Updater(token=bot_settings.BOT_TOKEN, use_context=True)
 my_bot.dispatcher.add_handler(CommandHandler("start", start))
 my_bot.dispatcher.add_handler(CommandHandler("status", status))
-my_bot.dispatcher.add_handler(CommandHandler("end", inviter_submit))
 my_bot.dispatcher.add_handler(CallbackQueryHandler(callback_handler))
 
 logger.info("* Start polling...")
